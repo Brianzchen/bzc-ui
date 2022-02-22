108676f63b87badbb139b10bc77dea7f3ef22f6f
Brian Chen <brian.chen@tabcorp.com.au>
2022-02-22 20:49:32 +1100

    add deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
   extends: ['eslint-config-bzc'],
   rules: {
+    'max-len': ['error', 100, 2, {
+      ignoreUrls: true,
+      ignoreComments: true,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+      ignorePattern: '^.*React\\$AbstractComponent.*',
+    }],
     'no-useless-computed-key': 0,
   },
 };
