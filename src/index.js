e4fa9b45ece33137c86fa3523f1e1633b4fe662e
Brian Chen <brian.chen@tabcorp.com.au>
2022-02-22 17:40:12 +1100

    prep for inital publish

---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.log('stars are falling soon!');
--- src/theme/colors/default.js
+++ src/theme/colors/default.js
@@ -1,30 +1,20 @@
 // @flow
-export const primary = '#1D2F5E';
+import type { ColorT } from '../../types';
 
-export const secondary = '#2B59C3';
-
-export const highlight = '#1d89bf';
-
-export const error = '#bf0000';
-
-export const successBackground = '#daf2e6';
-
-export const infoBackground = '#fff9dc';
-
-export const warningBackground = '#ffefe5';
-
-export const errorBackground = '#ffd9d9';
-
-export const grey1 = '#191919';
-
-export const grey2 = '#333333';
-
-export const grey3 = '#737373';
-
-export const grey4 = '#cccccc';
-
-export const grey5 = '#e5e5e5';
-
-export const grey6 = '#f2f2f2';
-
-export const grey7 = '#ffffff';
+export default ({
+  primary: '#1D2F5E',
+  secondary: '#2B59C3',
+  highlight: '#1d89bf',
+  error: '#bf0000',
+  successBackground: '#daf2e6',
+  infoBackground: '#fff9dc',
+  warningBackground: '#ffefe5',
+  errorBackground: '#ffd9d9',
+  grey1: '#191919',
+  grey2: '#333333',
+  grey3: '#737373',
+  grey4: '#cccccc',
+  grey5: '#e5e5e5',
+  grey6: '#f2f2f2',
+  grey7: '#ffffff',
+}: { [key: ColorT]: string });
