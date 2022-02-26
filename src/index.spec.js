fd66876d97a2b3b4eed14131c05ca4a6ba4ea17b
Brian Chen <brian.chen@tabcorp.com.au>
2022-02-26 11:25:07 +1100

    ensure there's awlays a top level export

---
+++ src/index.spec.js
@@ -0,0 +1,24 @@
+// @flow
+import fs from 'fs';
+import path from 'path';
+
+import * as starfall from '.';
+
+describe('starfall', () => {
+  const exclusions = ['internal', 'theme'];
+
+  it('exports all top level functions and components', () => {
+    const dirs = fs.readdirSync(__dirname);
+
+    dirs.forEach((dir) => {
+      const stat = fs.lstatSync(path.join(__dirname, dir));
+      if (stat.isDirectory() && !exclusions.includes(dir)) {
+        if (starfall[dir]) {
+          expect(starfall[dir]).toBeDefined();
+        } else {
+          throw new Error(`\`${dir}\` is missing a top level export`);
+        }
+      }
+    });
+  });
+});
